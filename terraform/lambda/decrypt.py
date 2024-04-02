56fdc7c22a46de69a448f28fad2aa53ab17523b5
Pat Heard <patrick.heard@cds-snc.ca>
2024-04-02 14:30:56 +0000

    feat: add formatting and tests

--- terraform/lambda/decrypt.py
+++ terraform/lambda/decrypt.py
@@ -12,34 +12,42 @@ from aws_encryption_sdk.internal.crypto import WrappingKey
 from aws_encryption_sdk.key_providers.raw import RawMasterKeyProvider
 from aws_encryption_sdk.identifiers import WrappingAlgorithm, EncryptionKeyType
 
-AWS_REGION = os.environ.get('AWS_REGION')
-RDS_ACTIVITY_STREAM_NAME = os.environ.get('RDS_ACTIVITY_STREAM_NAME')
-RDS_ACTIVITY_STREAM_ID = RDS_ACTIVITY_STREAM_NAME[len('aws-rds-das-'):]
+AWS_REGION = os.environ.get("AWS_REGION")
+RDS_ACTIVITY_STREAM_NAME = os.environ.get("RDS_ACTIVITY_STREAM_NAME")
+RDS_ACTIVITY_STREAM_ID = RDS_ACTIVITY_STREAM_NAME[len("aws-rds-das-") :]
 
-enc_client = aws_encryption_sdk.EncryptionSDKClient(commitment_policy=CommitmentPolicy.FORBID_ENCRYPT_ALLOW_DECRYPT)
-kms = boto3.client('kms', region_name=AWS_REGION)
+enc_client = aws_encryption_sdk.EncryptionSDKClient(
+    commitment_policy=CommitmentPolicy.FORBID_ENCRYPT_ALLOW_DECRYPT
+)
+kms = boto3.client("kms", region_name=AWS_REGION)
 logging.getLogger().setLevel(logging.ERROR)
+
 
 def handler(event, context):
     output = []
     logging.info(f"Received {len(event['records'])} Kinesis records to process...")
-    
-    for record in event['records']:
-        record_data = base64.b64decode(record['data'])
+
+    for record in event["records"]:
+        record_data = base64.b64decode(record["data"])
         record_data = json.loads(record_data)
-        payload_decoded = base64.b64decode(record_data['databaseActivityEvents'])
-        data_key_decoded = base64.b64decode(record_data['key'])
-        data_key_decrypt_result = kms.decrypt(CiphertextBlob=data_key_decoded, EncryptionContext={'aws:rds:dbc-id': RDS_ACTIVITY_STREAM_ID})
-        plain_text = decrypt_decompress(payload_decoded, data_key_decrypt_result['Plaintext'])
+        payload_decoded = base64.b64decode(record_data["databaseActivityEvents"])
+        data_key_decoded = base64.b64decode(record_data["key"])
+        data_key_decrypt_result = kms.decrypt(
+            CiphertextBlob=data_key_decoded,
+            EncryptionContext={"aws:rds:dbc-id": RDS_ACTIVITY_STREAM_ID},
+        )
+        plain_text = decrypt_decompress(
+            payload_decoded, data_key_decrypt_result["Plaintext"]
+        )
 
         output_record = {
-            'recordId': record['recordId'],
-            'result': 'Ok',
-            'data': base64.b64encode(plain_text).decode('utf-8')
+            "recordId": record["recordId"],
+            "result": "Ok",
+            "data": base64.b64encode(plain_text).decode("utf-8"),
         }
         output.append(output_record)
-        
-    return {'records': output}
+
+    return {"records": output}
 
 
 class MyRawMasterKeyProvider(RawMasterKeyProvider):
@@ -51,8 +59,11 @@ class MyRawMasterKeyProvider(RawMasterKeyProvider):
 
     def __init__(self, plain_key):
         RawMasterKeyProvider.__init__(self)
-        self.wrapping_key = WrappingKey(wrapping_algorithm=WrappingAlgorithm.AES_256_GCM_IV12_TAG16_NO_PADDING,
-                                        wrapping_key=plain_key, wrapping_key_type=EncryptionKeyType.SYMMETRIC)
+        self.wrapping_key = WrappingKey(
+            wrapping_algorithm=WrappingAlgorithm.AES_256_GCM_IV12_TAG16_NO_PADDING,
+            wrapping_key=plain_key,
+            wrapping_key_type=EncryptionKeyType.SYMMETRIC,
+        )
 
     def _get_raw_key(self, key_id):
         return self.wrapping_key
@@ -63,7 +74,10 @@ def decrypt_payload(payload, data_key):
     my_key_provider.add_master_key("DataKey")
     decrypted_plaintext, header = enc_client.decrypt(
         source=payload,
-        materials_manager=aws_encryption_sdk.materials_managers.default.DefaultCryptoMaterialsManager(master_key_provider=my_key_provider))
+        materials_manager=aws_encryption_sdk.materials_managers.default.DefaultCryptoMaterialsManager(
+            master_key_provider=my_key_provider
+        ),
+    )
     return decrypted_plaintext
 
 
